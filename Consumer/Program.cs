5d104115a57bd4360e00065dc5cbd2690471f716
Arsen <arsen.dmitriiev@gmail.com>
2026-06-13 06:11:01 +0300

    add migration

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -29,8 +29,8 @@ var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
-    //var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    //db.Database.Migrate();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
